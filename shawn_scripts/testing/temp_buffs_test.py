9da75035c862f4d9274f92e82de9da49c2247a3c
shla9937 <shla9937.colorado.edu>
2022-12-06 11:14:56 -0700

    fixed row issue

--- shawn_scripts/testing/temp_buffs_test.py
+++ shawn_scripts/testing/temp_buffs_test.py
@@ -5,3 +5,10 @@ for row in comp_row_list:
     for i in range(1,7):
         which_compounds.append(row+str(i))
 print(which_compounds)
+
+which_rows = 0
+for i in range(which_rows, which_rows+16, 2):
+    print(i)
+which_rows = 1
+for i in range(which_rows, which_rows+16, 2):
+    print(i)
