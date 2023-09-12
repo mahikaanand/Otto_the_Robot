ff344631acb495cce98d3aea93614f052e47f364
shla9937 <shla9937@colordao.edu>
2023-09-12 11:39:19 -0600

    48well

--- shawn_scripts/testing/48well_test.py
+++ shawn_scripts/testing/48well_test.py
@@ -13,13 +13,13 @@ metadata = {
 def run(protocol):
     #setup
     tips300 = protocol.load_labware('opentrons_96_tiprack_300ul', 5)
-    plate48 = protocol.load_labware('hampton_48_wellplate_200ul.json', 6)
+    plate48 = protocol.load_labware('hampton_48_wellplate_200ul', 6)
     p300m = protocol.load_instrument('p300_multi_gen2', 'left',
                                      tip_racks=[tips300])
     p300m.pick_up_tip()
-    p300m.transfer(20,
-                   plate48.rows()[0][0:6],
-                   plate48.rows()[0][1:7], 
+    p300m.transfer(20, 
+                   plate48.rows()[0][0:4],
+                   plate48.rows()[0][1:5], 
                    new_tip='never')
     p300m.drop_tip()
  
