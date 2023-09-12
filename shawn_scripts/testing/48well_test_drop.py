68c268089d99172a5afdb18521281f4a44bc8598
shla9937 <shla9937@colorado.edu>
2023-09-12 11:56:58 -0600

    drop

--- shawn_scripts/testing/48well_test_drop.py
+++ shawn_scripts/testing/48well_test_drop.py
@@ -27,12 +27,23 @@ def run(protocol):
     #                plate48_well.rows()[0][1:5], 
     #                new_tip='never')
     # p300m.drop_tip()
-
+    strobe(12, 8, True, protocol)
     p20m.pick_up_tip()
     p20m.transfer(2, 
                    plate48_drop.rows()[0][0:4],
                    plate48_drop.rows()[0][1:5], 
                    new_tip='never')
     p20m.drop_tip()
+    strobe(12, 8, False, protocol)
+
+def strobe(blinks, hz, leave_on, protocol):
+    i = 0
+    while i < blinks:
+        protocol.set_rail_lights(True)
+        time.sleep(1/hz)
+        protocol.set_rail_lights(False)
+        time.sleep(1/hz)
+        i += 1
+    protocol.set_rail_lights(leave_on)
     
  
