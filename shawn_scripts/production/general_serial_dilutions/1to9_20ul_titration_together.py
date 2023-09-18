17488ea9fd35b49eb4faa2c1e4802d1d8eb3622f
shla9937 <shla9937@colorado.edu>
2023-09-18 11:42:20 -0600

    added well targets

--- shawn_scripts/production/general_serial_dilutions/1to9_20ul_titration_together.py
+++ shawn_scripts/production/general_serial_dilutions/1to9_20ul_titration_together.py
@@ -13,12 +13,13 @@ metadata = {
     }
 
 def run(protocol):
-    well_96start = 0 #index from 0
+    well_96startx = 1 #index from 1 (as labelled on plate)
+    well_96starty = A #letter of row in 96well plate
     num_samples = 2
     dilution = 10 #1 in this dilution factor
 
     strobe(12, 8, True, protocol)
-    setup(well_96start, num_samples, protocol)
+    setup(well_96startx, well_96starty, num_samples, protocol)
     titrate_together(dilution, protocol)
     strobe(12, 8, False, protocol)
 
@@ -32,7 +33,7 @@ def strobe(blinks, hz, leave_on, protocol):
         i += 1
     protocol.set_rail_lights(leave_on)
 
-def setup(well_96start, num_samples, protocol):
+def setup(well_96startx, well_96starty, num_samples, protocol):
     #equiptment
     global trash, tips20, tips300, p20m, p300m, plate96, tempdeck, temp_pcr
     trash = protocol.fixed_trash['A1']
@@ -47,8 +48,10 @@ def setup(well_96start, num_samples, protocol):
     temp_pcr = tempdeck.load_labware(
                  'opentrons_96_aluminumblock_generic_pcr_strip_200ul')
 
-    global start_96well, samples
-    start_96well = well_96start
+    global start_96wellx, start_96welly, samples
+    start_96wellx = well_96startx-1
+    row_dict = {A:0, B:1, C:2, D:3, E:4, F:5, G:6, H:7}
+    start_96welly = row_dict[well_96starty]
     samples = num_samples
 
     #single tips
@@ -86,19 +89,19 @@ def pickup_tips(number, pipette, protocol):
         tip300 += 1
 
 def titrate_together(dilution, protocol):
-    enzy_col = start_96well
+    enzy_col = start_96wellx
     buff_col = enzy_col+1
     samp_col = buff_col+1
     
     #add buff
     pickup_tips(samples, p300m, protocol)
-    p300m.aspirate(175, plate96.rows()[0][buff_col])
+    p300m.aspirate(175, plate96.rows()[start_96welly][buff_col])
     for i in range(1,7):
         p300m.dispense(20, temp_pcr.rows()[0][i])
 
     #titrate
-    p300m.aspirate(20+(20/dilution), plate96.rows()[0][enzy_col])
-    p300m.dispense(20+(20/dilution), temp_pcr.rows()[0][0])    
+    p300m.aspirate(20+(20/dilution), plate96.rows()[start_96welly][enzy_col])
+    p300m.dispense(20+(20/dilution), temp_pcr.rows()[start_96welly][0])    
     p300m.drop_tip()
     pickup_tips(samples, p20m, protocol)
     for i in range(0,5):
