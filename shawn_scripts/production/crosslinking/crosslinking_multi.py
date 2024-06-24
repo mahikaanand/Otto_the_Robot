2d968ff5808d59556679e8de4ba75b1ef78663ca
shla9937 <shla9937@colordao.edu>
2024-06-24 14:01:56 -0600

    updated

--- shawn_scripts/production/crosslinking/crosslinking_multi.py
+++ shawn_scripts/production/crosslinking/crosslinking_multi.py
@@ -10,10 +10,11 @@ metadata = {
     'description': '''This protocol will do a crosslinking reaction at 3 concentrations of BS3.
                       Liquids:
                       - Put 50µL of sample into pcr tray (every third column)
-                      - Fill up 1ml tube of buff in first position
-                      - Fill up >(10µL * num samples) 1ml tube of 2x BS3 in second position
-                      - Fill up >(10µL * num samples) tube of 2000x BS3 in third position
-                      - Fill up >(40µL * num samples) tube of SDS buff in fourth position
+                      - Fill up 1ml tube of buff in first position of 24well
+                      - Fill up >(10µL * num samples) 1ml tube of 2x BS3 in second position of 24well
+                      - Fill up >(10µL * num samples) tube of 2000x BS3 in third position of 24well
+                      - Fill up >(20µL * 3 * num samples) tube of glycine first position of trough
+                      - Fill up >(40µL * 3 * num samples) tube of SDS second position of trough
                       ''',
     'apiLevel': '2.18'
     }
@@ -107,8 +108,8 @@ def run(protocol):
     setup(protocol)
     
     distribute_samples(protocol)
-    add_crosslinker(protocol)
-    incubate(incubation_temp, incubation_time, protocol)
+    add_crosslinker(incubation_temp, protocol)
+    incubate(incubation_time, protocol)
     quench(quench_temp, quench_time, protocol)
     add_sample_buff(protocol)
     denature(denature_temp, denature_time, protocol)
@@ -127,19 +128,21 @@ def strobe(blinks, hz, leave_on, protocol):
 
 def setup(protocol):
     # equiptment
-    global tips300, tips20, trough, p300m, p20m, tempdeck, temp_pcr, rt_24, glycine
+    global tips300, tips20, tips20_2, trough, p300m, p20m, tempdeck, temp_pcr, rt_24, glycine, sds
     tips300 = protocol.load_labware('opentrons_96_tiprack_300ul', 6)
-    tips20 = protocol.load_labware('opentrons_96_tiprack_20ul', 5)
-    trough = protocol.load_labware('nest_12_reservoir_15ml', 4)
+    tips20 = protocol.load_labware('opentrons_96_tiprack_20ul', 4)
+    tips20_2 = protocol.load_labware('opentrons_96_tiprack_20ul', 5)
+    trough = protocol.load_labware('nest_12_reservoir_15ml', 2)
     p300m = protocol.load_instrument('p300_multi_gen2', 'left',
                                      tip_racks=[tips300])
     p20m = protocol.load_instrument('p20_multi_gen2', 'right',
-                                     tip_racks=[tips20])
+                                     tip_racks=[tips20, tips20_2])
     tempdeck = protocol.load_module('temperature module gen2', 10)
     temp_pcr = tempdeck.load_labware(
                  'opentrons_96_aluminumblock_generic_pcr_strip_200ul')
-    rt_24 = protocol.load_labware('opentrons_24_tuberack_eppendorf_1.5ml_safelock_snapcap', 7)
+    rt_24 = protocol.load_labware('opentrons_24_tuberack_eppendorf_1.5ml_safelock_snapcap', 8)
     glycine = trough.wells()[0]
+    sds = trough.wells()[1]
 
     # liquids
     yellowSamples = protocol.define_liquid(
@@ -157,7 +160,7 @@ def setup(protocol):
         description="Glycine",
         display_color="#00FF00",
     )
-    glycine.load_liquid(liquid=greenGlycine, volume=2000)
+    glycine.load_liquid(liquid=greenGlycine, volume=5000)
 
     grayBuff = protocol.define_liquid(
         name="Buff",
@@ -185,7 +188,7 @@ def setup(protocol):
         description="SDS buff",
         display_color="#0000FF",
     )
-    rt_24.rows()[0][3].load_liquid(liquid=blueSDS, volume=1600)
+    sds.load_liquid(liquid=blueSDS, volume=5000)
 
     #single tips
     global which_tips20, tip20
@@ -210,7 +213,10 @@ def pickup_tips(number, pipette, protocol):
             while (tip20 % 8) != 0:
                 tip20 += 1
         tip20 += number-1
-        p20m.pick_up_tip(tips20[which_tips20[tip20]])
+        if tip20 < 96:
+            p20m.pick_up_tip(tips20[which_tips20[tip20]])
+        else:
+            p20m.pick_up_tip(tips20_2[which_tips20[tip20-96]])
         tip20 += 1 
     if pipette == p300m:
         if (tip300 % number) != 0:
@@ -243,70 +249,104 @@ def distribute_samples(protocol):
                         disposal_volume=0, new_tip='never')
         p20m.drop_tip()
 
-def add_crosslinker(protocol):
+def add_crosslinker(incubation_temp, protocol):
     # add 0, 1, 1000x crosslinker
+    tempdeck.set_temperature(celsius=incubation_temp)
+    global start_time
+    start_time = time.time()
     for i in range(0, num_samples):
-        col = ((num_samples // 8)*3) + temp_96start 
-        row = num_samples % 8
+        col = ((i // 8)*3) + temp_96start 
+        row = i % 8
         pickup_tips(1, p20m, protocol)
-        p20m.aspirate(10,rt_24.rows()[0][0])
-        p20m.dispense(10,temp_pcr.rows()[col][row])
+        p20m.aspirate(10,rt_24.rows()[0][0].bottom(10))
+        p20m.dispense(10,temp_pcr.rows()[row][col])
         p20m.mix(3,10)
         p20m.drop_tip()
         pickup_tips(1, p20m, protocol)
-        p20m.aspirate(10,rt_24.rows()[0][1])
-        p20m.dispense(10,temp_pcr.rows()[col+1][row])
+        p20m.aspirate(10,rt_24.rows()[0][1].bottom(10))
+        p20m.dispense(10,temp_pcr.rows()[row][col+1])
         p20m.mix(3,10)
         p20m.drop_tip()
         pickup_tips(1, p20m, protocol)
-        p20m.aspirate(10,rt_24.rows()[0][2])
-        p20m.dispense(10,temp_pcr.rows()[col+2][row])
+        p20m.aspirate(10,rt_24.rows()[0][2].bottom(10))
+        p20m.dispense(10,temp_pcr.rows()[row][col+2])
         p20m.mix(3,10)
         p20m.drop_tip()
 
-def incubate(incubation_temp, incubation_time, protocol):
-    tempdeck.set_temperature(celsius=incubation_temp)
-    protocol.delay(minutes=incubation_time)
+def incubate(incubation_time, protocol):
+    end_time = start_time + incubation_time*60
+    try:
+        if not protocol.is_simulating():
+            while time.time() < end_time:
+                time.sleep(1)
+        else:
+            print('Not waiting, simulating')
+    except KeyboardInterrupt:
+        pass
+        print()
 
 def quench(quench_temp, quench_time, protocol):
     # do it slowly to get equal time on heat block
     for i in range(0, num_samples):
-        col = ((num_samples // 8)*3) + temp_96start 
-        row = num_samples % 8
+        col = ((i // 8)*3) + temp_96start 
+        row = i % 8
         pickup_tips(1, p20m, protocol)
         p20m.aspirate(10,glycine)
-        p20m.dispense(10,temp_pcr.rows()[col][row])
+        p20m.dispense(10,temp_pcr.rows()[row][col])
         p20m.mix(3,20)
         p20m.drop_tip()
         pickup_tips(1, p20m, protocol)
         p20m.aspirate(10,glycine)
-        p20m.dispense(10,temp_pcr.rows()[col+1][row])
+        p20m.dispense(10,temp_pcr.rows()[row][col+1])
         p20m.mix(3,20)
         p20m.drop_tip()
         pickup_tips(1, p20m, protocol)
         p20m.aspirate(10,glycine)
-        p20m.dispense(10,temp_pcr.rows()[col+2][row])
+        p20m.dispense(10,temp_pcr.rows()[row][col+2])
         p20m.mix(3,20)
         p20m.drop_tip()
 
 def add_sample_buff(protocol):   
-    for i in range(0, num_samples):
-        col = ((num_samples // 8)*3) + temp_96start 
-        row = num_samples % 8
-        pickup_tips(1, p300m, protocol)
-        p300m.aspirate(40,rt_24.rows()[0][3])
-        p300m.dispense(40,temp_pcr.rows()[col][row])
-        p300m.mix(3,40)
+    # full 8 cols
+    for i in range(0, (num_samples // 8)):
+        col = (i*3) + temp_96start
+        pickup_tips(8, p300m, protocol)
+        p300m.distribute(40, sds, 
+                      temp_pcr.rows()[0][col],
+                      disposal_volume=0, new_tip='never')
         p300m.drop_tip()
-        pickup_tips(1, p300m, protocol)
-        p300m.aspirate(40,rt_24.rows()[0][3])
-        p300m.dispense(40,temp_pcr.rows()[col+1][row])
-        p300m.mix(3,40)
+        pickup_tips(8, p300m, protocol)
+        p300m.distribute(40, sds, 
+                      temp_pcr.rows()[0][col+1],
+                      disposal_volume=0, new_tip='never')
         p300m.drop_tip()
-        pickup_tips(1, p300m, protocol)
-        p300m.aspirate(40,rt_24.rows()[0][3])
-        p300m.dispense(40,temp_pcr.rows()[col+2][row])
-        p300m.mix(3,40)
+        pickup_tips(8, p300m, protocol)
+        p300m.distribute(40, sds, 
+                      temp_pcr.rows()[0][col+2],
+                      disposal_volume=0, new_tip='never')
+        p300m.drop_tip()
+
+    # remainder rows in last col
+    remainder = num_samples % 8
+    if remainder != 0:
+        try:
+            col += 3
+        except:
+            col = 0
+        pickup_tips(8, p300m, protocol)
+        p300m.distribute(40, sds, 
+                      temp_pcr.rows()[0][col],
+                      disposal_volume=0, new_tip='never')
+        p300m.drop_tip()
+        pickup_tips(8, p300m, protocol)
+        p300m.distribute(40, sds, 
+                      temp_pcr.rows()[0][col+1],
+                      disposal_volume=0, new_tip='never')
+        p300m.drop_tip()
+        pickup_tips(8, p300m, protocol)
+        p300m.distribute(40, sds, 
+                      temp_pcr.rows()[0][col+2],
+                      disposal_volume=0, new_tip='never')
         p300m.drop_tip()
 
 def denature(denature_temp, denature_time, protocol):
